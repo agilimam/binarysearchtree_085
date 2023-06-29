389ff5cf12814a269d919cbfb55fc8434fe8fe23
agilimam <agilimamalhafizi04@gmail.com>
2023-06-29 21:01:46 +0700

    membuat case 2

--- Binary_search_tree/Binary_search_tree.cpp
+++ Binary_search_tree/Binary_search_tree.cpp
@@ -138,11 +138,15 @@ int main()
 		switch (ch)
 		{
 		case '1':
+		{
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
 		}
-		cout << "Enter a word: ";
-		string word;
-		cin >> word;
-		obj.insert(word);
-		break;
-	}
-}
+		case'2':
+		{
+			obj.inorder(obj.ROOT);
+			break;
+		}
